505c70df9cbdb128b5824207ecee371588be148f
Furkan Yolcu <furkan.yolcu@Irem-iMac.local>
2025-09-26 10:38:45 +0300

    furkan yolcu commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-  int furkan = 2;
-  int semih = 2;
+
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -56,14 +55,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  int furkan = 2;
+  int semih = 2;
+  int fatih = 12;
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.git checkout develop
       _counter++;
     });
   }
